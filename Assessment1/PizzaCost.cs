d225d074114fd0b7c6b6fcc6a896cd2abd27f2bc
u26q51 <u26q51@INGURWN120063>
2023-11-03 09:58:52 +0530

    Add project files.

---
+++ Assessment1/PizzaCost.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assessment1
+{
+    internal class PizzaCost
+    {
+        static void Main()
+            {
+                List<Pizza> pizzaOrders = new List<Pizza>();
+
+                
+                Pizza Mediumpizza1 = new Pizza(1, "Medium",5, 7);
+                Pizza Mediumpizza2 = new Pizza(5, "Medium", 8, 1);
+
+                pizzaOrders.Add(Mediumpizza1);
+                pizzaOrders.Add(Mediumpizza2);
+
+               
+                foreach (Pizza pizza in pizzaOrders)
+                {
+                    Console.WriteLine(pizza.GetDescription());
+                    Console.WriteLine("Cost: $" + pizza.CalcCost());
+                   
+                }
+            }
+
+        }
+}
---
+++ Assessment1/PizzaDetail.cs
@@ -0,0 +1,92 @@
+﻿namespace Assessment1
+{
+  public class Pizza
+        {
+           
+            private int cheeseToppings;
+        private int pepperoniToppings;
+        private string size;
+        private int hamToppings;
+
+
+            public int CheeseToppings
+            {
+                get { return cheeseToppings; }
+                set { cheeseToppings = value; }
+            }
+
+            public int PepperoniToppings
+            {
+                get 
+               {
+                return pepperoniToppings;
+                }
+                set
+            { 
+                pepperoniToppings = value; 
+            }
+            }
+
+        public string Size
+        {
+            get
+            {
+                return size;
+            }
+            set
+            {
+                size = value;
+            }
+        }
+
+        public int HamToppings
+            {
+                get
+            { 
+                return hamToppings;
+            }
+                set 
+            { 
+                hamToppings = value; 
+            }
+            }
+
+            public Pizza( int cheeseToppings, string size, int pepperoniToppings, int hamToppings)
+            {
+             
+                this.cheeseToppings = cheeseToppings;
+            this.size = size;
+                  this.pepperoniToppings = pepperoniToppings;
+                this.hamToppings = hamToppings;
+            }
+
+            public double CalcCost()
+            {
+                double cost = 0;
+
+                   switch (size)
+                {
+                          case "Small":
+                        cost = 10 + 2 * ( pepperoniToppings + cheeseToppings+ hamToppings);
+                        break;
+                        case "Medium":
+                        cost = 12 + 2 * (pepperoniToppings + cheeseToppings + hamToppings);
+                        break;
+                    case "Large":
+                        cost = 14 + 2 * (pepperoniToppings + cheeseToppings + hamToppings);
+                        break;
+                    default:
+                        break;
+                }
+
+                return cost;
+            }
+
+            public string GetDescription()
+            {
+                return $"{size} Pizza with {cheeseToppings} cheese topping, {pepperoniToppings} pepperoni topping, and {hamToppings} ham topping";
+            }
+        }
+
+    
+}
